c34b51a9c52772f0109f66d93acf615d5af18f32
elenal.gonzalez <elenal.gonzalez@estudiante.uam.es>
2024-04-28 04:26:14 +0200

    domain

--- tourGPT/settings.py
+++ tourGPT/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tourGPT.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tourgpt-kvas.onrender.com', 'tourgpt.com', '127.0.0.1']
 
 
 # Application definition
